ccf6461605536680e4bb8d1ba90887fb6c4576ce
OFHATWA <tofhatwa@gmail.com>
2023-10-17 21:30:23 +0200

    k

--- one_zero.c
+++ one_zero.c
@@ -8,51 +8,69 @@
 
 int _printf(const char *format, ...)
 {
-	int length = 0;
-	va_list index;
+	char *output_ptr;
+	char *output;
+	int total_characters;
+	char *str;
+	va_list args;
+	va_start(args, format);
 
-	if (format  == NULL)
-		return (-1);
+	total_characters = 0;
 
-	va_start(index, format);
-
-	while (*format)
+	output = (char *)malloc(1000 * sizeof(char)); 
+	
+	if (output == NULL) 
 	{
-		if (*format != '%')
+		perror("Memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	output_ptr = output;
+	
+	while (*format) 
+	{
+		if (*format == '%')
 		{
-			write(1, format, 1);
-			length++;
+			format++; 
+			if (*format == 'c') 
+			{
+				char c = (char)va_arg(args, int);
+				*output_ptr = c;
+				output_ptr++;
+				total_characters++;
+			} else if (*format == 's') 
+			{
+				str = va_arg(args, char *);
+
+				while (*str)
+				{
+					*output_ptr = *str;
+					output_ptr++;
+					str++;
+					total_characters++;
+				}
+			}
+			else if (*format == '%') 
+			{
+				*output_ptr = '%';
+				output_ptr++;
+				total_characters++;
+			}
 		}
 		else
 		{
-			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				length++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(index, char*);
-				int length1 = 0;
-
-				while (str[length1] != '\0')
-					length1++;
-				write(1, str, length1);
-				length += length1;
-			}
-			else if (*format == 'c')
-			{
-				char letter = va_arg(index, int);
-
-				write(1, &letter, 1);
-				length++;
-			}
+			*output_ptr = *format;
+			output_ptr++;
+			total_characters++;
 		}
 		format++;
 	}
-	va_end(index);
-	return (length - 1);
+
+	*output_ptr = '\0';
+	printf("%s", output);
+	
+	free(output);
+	
+	va_end(args);
+	return (total_characters);
 }
