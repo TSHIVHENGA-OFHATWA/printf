b681d59383c90997909a24c88ef280fed43a1adc
OFHATWA <tofhatwa@gmail.com>
2023-10-17 22:44:02 +0200

    1

---
+++ number_one.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * form_char - print char characater.
+ * @index: list of argments
+ * Return: the number of characters from string.
+ */
+int form_char(va_list index)
+{
+	int length = 1;
+	char character = va_arg(index, int);
+
+	putchar(character);
+	length++;
+	return (length);
+}
+/**
+ * form_str - print string characater.
+ * @index: list of argments
+ * Return: the number of characters from string.
+ */
+int form_str(va_list index)
+{
+	int length1 = 0;
+	char *str;
+
+	str = va_arg(index, char*);
+
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+		length1++;
+	}
+	return (length1);
+}
+
+/**
+ * _printf - produces output according to a format.
+ * @format: to be printed
+ * Return: the number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	va_list index;
+	int length1, length = 0;
+
+	va_start(index, format);
+
+	if (format == NULL)
+		return (-1);
+	for (; *format != '\0'; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			else if (*format == 'c')
+			{
+				length = form_char(index);
+			}
+			else if (*format == 's')
+			{
+				length1 = form_str(index);
+				length += length1;
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				length++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			length += 2;
+		}
+	}
+	va_end(index);
+	return (length - 1);
+}
